a807b2a4f4351f23795b67c58c25e50405de1649
Christian Lewe <ch_lewe@agdsn.me>
2023-07-30 12:26:49 +0200

    Show dlog in finite field plot
    This revealed a bug where imshow plots rotates the curve due to swapped
    x and y coordinates. Now the finite field plot matches the hardness of
    dlog plot.

--- plot_curve_finite.py
+++ plot_curve_finite.py
@@ -3,16 +3,25 @@ import numpy as np
 from ec.static import XY
 from ec.core import MAX_COORDINATE, PARAMETER_A, PARAMETER_B
 
+# Initialize the plot
+fig, ax = plt.subplots()
+
 # Initialize an array of zeros (white squares)
 points = np.zeros((MAX_COORDINATE, MAX_COORDINATE))
 
 # Add points on the curve
-for xy in XY:
+for (dlog, xy) in enumerate(XY):
     if xy:
-        points[xy[0], xy[1]] = 1
+        # Inverted coordinates
+        # because imshow treats first component as rows (y coordinate)
+        # and second component as columns (x coordinate)
+        points[xy[1], xy[0]] = 1
+        ax.text(xy[0], xy[1], str(dlog), ha="center", va="center", color="white")
 
 # Plot the curve
-plt.imshow(points, cmap='gray_r', origin='lower')
+# Set origin to the bottom left
+# because imshow places it at the top left by default
+ax.imshow(points, cmap='gray_r', origin='lower')
 
-plt.title("Elliptic curve $y^2 \equiv x^3 + {}x + {}$ (mod {})".format(PARAMETER_A.value, PARAMETER_B.value, MAX_COORDINATE))
+ax.set_title("Elliptic curve $y^2 \equiv x^3 + {}x + {}$ (mod {})".format(PARAMETER_A.value, PARAMETER_B.value, MAX_COORDINATE))
 plt.show()
