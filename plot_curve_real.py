ec0848b66202e06baecb5a3c2a1e090b76e57f87
Christian Lewe <ch_lewe@agdsn.me>
2023-07-30 12:00:22 +0200

    Plot curve in reals

---
+++ plot_curve_real.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ec.core import PARAMETER_A, PARAMETER_B
+
+# Generate x values
+x = np.linspace(-2, 2, 400)
+
+# Calculate y values (note: this will include complex numbers)
+y_squared = x**3 + PARAMETER_A.value * x + PARAMETER_B.value
+
+# We'll take the real square roots as y values, and ignore the complex roots
+y_positive = np.real(np.sqrt(y_squared))
+y_negative = -np.real(np.sqrt(y_squared))
+
+# Plot the curve
+plt.plot(x, y_positive, "b")
+plt.plot(x, y_negative, "b")
+
+plt.title("Elliptic curve $y^2 = x^3 + {}x + {}$ over ℝ".format(PARAMETER_A.value, PARAMETER_B.value))
+plt.grid(True)
+plt.show()
