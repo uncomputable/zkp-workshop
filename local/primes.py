6ad6aa31c84a42052d5ad9952bfa0bd1b4e33b00
Christian Lewe <ch_lewe@agdsn.me>
2023-08-29 11:16:33 +0200

    Primes: Add Z*_N tools
    Compute the Euler totient function and (random) coprimes for the
    multiplicative group of integers modulo n.

--- local/primes.py
+++ local/primes.py
@@ -1,3 +1,6 @@
+from typing import Iterable, List
+from functools import reduce
+from operator import mul
 from local.ec.core import MAX_COORDINATE, NUMBER_POINTS
 import random
 import math
@@ -82,7 +85,51 @@ def is_prime(n: int) -> bool:
     return miller_rabin(n, 5)
 
 
-class TestIsPrime(unittest.TestCase):
+def euler_totient(factors: Iterable[int]) -> int:
+    """
+    Compute Euler's totient function of integer n whose prime factorization is given.
+
+    **The method assumes that the given factors are prime numbers!**
+    """
+    n = reduce(mul, factors, 1)
+    return int(n * reduce(mul, [1 - 1 / p for p in set(factors)], 1))
+
+
+def is_coprime(a: int, factors: Iterable[int]) -> bool:
+    """
+    Check if integer a is a coprime of integer n whose prime factorization is given.
+
+    **The method assumes that the given factors are prime numbers!**
+    """
+    return all(a % p != 0 for p in factors)
+
+
+def get_coprimes(factors: Iterable[int]) -> List[int]:
+    """
+    Return the list of coprimes of integer n whose prime factorization is given.
+
+    This is equivalent to the elements of the multiplicative group of integers modulo n.
+
+    **The method assumes that the given factors are prime numbers!**
+    """
+    n = reduce(mul, factors, 1)
+    return [a for a in range(1, n) if is_coprime(a, factors)]
+
+
+def get_coprime(factors: Iterable[int]) -> int:
+    """
+    Return a random coprime of integer n whose prime factorization is given.
+
+    **The method assumes that the given factors are prime numbers!**
+    """
+    n = reduce(mul, factors, 1)
+    while True:
+        a = random.randrange(1, n)
+        if is_coprime(a, factors):
+            return a
+
+
+class TestPrimes(unittest.TestCase):
     primes = [
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
         101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197,
@@ -93,6 +140,12 @@ class TestIsPrime(unittest.TestCase):
         701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829,
         839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977,
         983, 991, 997
+    ]
+    euler_totients = [
+        1, 1, 2, 2, 4, 2, 6, 4, 6, 4, 10, 4, 12, 6, 8, 8, 16, 6, 18, 8, 12, 10, 22, 8, 20, 12, 18, 12, 28, 8, 30, 16,
+        20, 16, 24, 12, 36, 18, 24, 16, 40, 12, 42, 20, 24, 22, 46, 16, 42, 20, 32, 24, 52, 18, 40, 24, 36, 28, 58, 16,
+        60, 30, 36, 32, 48, 20, 66, 32, 44, 24, 70, 24, 72, 36, 40, 36, 60, 24, 78, 32, 54, 40, 82, 24, 64, 42, 56, 40,
+        88, 24, 72, 44, 60, 46, 72, 32, 96, 42, 60, 40
     ]
 
     def test_trial_division(self):
@@ -119,3 +172,26 @@ class TestIsPrime(unittest.TestCase):
 
     def test_number_points_is_prime(self):
         self.assertTrue(is_prime(NUMBER_POINTS))
+
+    def test_euler_totient(self):
+        primes = [p for p in self.primes if p < 100]
+
+        for a in primes:
+            self.assertEqual(self.euler_totients[a - 1], euler_totient([a]))
+            for b in primes:
+                if a * b > 100:
+                    break
+                self.assertEqual(self.euler_totients[a * b - 1], euler_totient([a, b]))
+                for c in primes:
+                    if a * b * c > 100:
+                        break
+                    self.assertEqual(self.euler_totients[a * b * c - 1], euler_totient([a, b, c]))
+
+    def test_get_coprimes(self):
+        primes = [p for p in self.primes if p < 100]
+
+        for a in primes:
+            for b in primes:
+                n = a * b
+                z_n_star = [i for i in range(n) if math.gcd(i, n) == 1]
+                self.assertEqual(z_n_star, get_coprimes([a, b]))
