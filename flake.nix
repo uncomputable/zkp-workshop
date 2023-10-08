816d991c290fa7e6247fdf38554721a5ebb62306
Christian Lewe <ch_lewe@agdsn.me>
2023-10-08 12:06:49 +0200

    Nix: Support default systems
    Sage is not supported by MacOS, so the sage shell is not usable on Mac.
    In the future, we could try to make the sage shell available only on
    supported systems which would give Mac users a clue that they cannot use
    it on their system.
    
    Also update the flake lock file.

--- flake.nix
+++ flake.nix
@@ -3,42 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
+    flake-utils
   }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    python = pkgs.python3;
-    coreDeps = with python.pkgs; [
-      matplotlib
-      numpy
-      notebook
-      networkx
-      ipywidgets
-      pygame
-    ];
-    scipyDeps = with python.pkgs; coreDeps ++ [
-      scipy
-    ];
-    makePythonShell = deps: pkgs.mkShell {
-      packages = [
-        (python.withPackages (ps: deps))
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      python = pkgs.python3;
+      coreDeps = with python.pkgs; [
+        matplotlib
+        numpy
+        notebook
+        networkx
+        ipywidgets
+        pygame
       ];
-    };
-    makeSageShell = deps: pkgs.mkShell {
-      packages = [
-        pkgs.sage
+      scipyDeps = with python.pkgs; coreDeps ++ [
+        scipy
       ];
-    };
-  in
-  {
-    devShells.x86_64-linux = {
-      default = makePythonShell coreDeps;
-      scipy = makePythonShell scipyDeps;
-      sage = makeSageShell [];
-    };
-  };
+      makePythonShell = deps: pkgs.mkShell {
+        packages = [
+          (python.withPackages (ps: deps))
+        ];
+      };
+      makeSageShell = deps: pkgs.mkShell {
+        packages = pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [
+          pkgs.sage
+        ];
+      };
+    in
+    {
+      devShells = {
+        default = makePythonShell coreDeps;
+        scipy = makePythonShell scipyDeps;
+        sage = makeSageShell [];
+      };
+    }
+  );
 }
